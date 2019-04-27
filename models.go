d138a748d0b53ae326cd4a1cb1b6fda55d96e8f5
Apakhov <m_apakhov@mail.ru>
2019-04-27 11:11:54 +0000

    [ADD] MessageAccessObject implementation

--- models.go
+++ models.go
@@ -3,14 +3,11 @@ package main
 import (
 	"github.com/jackc/pgx"
 	"github.com/jackc/pgx/pgtype"
+	"github.com/pkg/errors"
 )
 
 // conn коннект к базе данных
 var pgxConn *pgx.ConnPool
-
-type Queryer interface {
-	QueryRow(string, ...interface{}) *pgx.Row
-}
 
 // MessageAccessObject DAO for User model
 type MessageAccessObject interface {
@@ -28,18 +25,54 @@ func init() {
 	Messages = &AccessObject{}
 }
 
-// User model for users table
+// MessageModel Message model for users table
 type MessageModel struct {
-	ID       pgtype.Int8
-	message  pgtype.Text
-	authorID pgtype.Int8
-	convID   pgtype.Int8
+	ID      pgtype.Int8
+	Message pgtype.Text
+	Author  pgtype.Text
+	ConvID  pgtype.Int8
+}
+
+func (ms *AccessObject) Create(m *MessageModel) error {
+	tx, err := pgxConn.Begin()
+	if err != nil {
+		return errors.Wrap(err, "can not open message create transaction")
+	}
+	defer tx.Rollback()
+
+	_, err = tx.Exec(`INSERT INTO messages (message, author, conv_id) VALUES($1, $2, $3);`, &m.Message, &m.Author, &m.ConvID)
+	if err != nil {
+		return errors.Wrap(err, "message create error")
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return errors.Wrap(err, "message create transaction commit error")
+	}
+
+	return nil
+
 }
 
 func (ms *AccessObject) GetMessagesByConvID(id int64, limit int, offset int) ([]*MessageModel, error) {
+	tx, err := pgxConn.Begin()
+	if err != nil {
+		return nil, errors.Wrap(err, "can not open messages get transaction")
+	}
+	defer tx.Rollback()
 
-}
+	rows, err := tx.Query(`SELECT m.id, m.message, m.author, m.conv_id FROM messages m WHERE conv_id = $1 LIMIT $2 OFFSET $3`, id, limit, offset)
+	msgs := make([]*MessageModel, 0, 0)
 
-func (ms *AccessObject) Create(u *MessageModel) error {
+	for rows.Next() {
+		m := &MessageModel{}
+		err := rows.Scan(&m.ID, &m.Message, &m.Author, &m.ConvID)
+		if err != nil {
+			return nil, errors.Wrap(err, "faild to get messages")
+		}
 
+		msgs = append(msgs, m)
+	}
+
+	return msgs, nil
 }
