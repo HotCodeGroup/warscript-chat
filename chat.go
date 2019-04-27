a6008b281e47a3ea43ff8f056c70cd3892f6dbd4
GDVFox <gaaveer@gmail.com>
2019-04-27 17:07:39 +0300

    [ADD] dev

--- chat.go
+++ chat.go
@@ -20,6 +20,8 @@ var upgrader = websocket.Upgrader{
 
 // SessionInfo достаёт инфу о юзере из контекстаs
 func GetSessionInfo(r *http.Request) (*models.SessionPayload, error) {
+	logger.Infof("%+v", r.Cookies())
+
 	cookie, err := r.Cookie("JSESSIONID")
 	if err != nil || cookie == nil {
 		return nil, errors.Wrap(err, "no cookie")
