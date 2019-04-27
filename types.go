9f5c38a4ff6478e6c98634f58c2cf1ec1bfcc384
Apakhov <m_apakhov@mail.ru>
2019-04-27 08:42:29 +0000

    [ADD] type descriptions

--- types.go
+++ types.go
@@ -4,19 +4,23 @@ import (
 	"encoding/json"
 )
 
+// MessageFromClient simple anon message
 type MessageFromClient struct {
 	Message string `json:"message"`
 }
 
+// MessageToClient simple anon message
 type MessageToClient struct {
 	Message string `json:"message"`
 }
 
+// WSObject object for ws connections
 type WSObject struct {
 	Type    string          `json:"string"`
 	Payload json.RawMessage `json:"payload"`
 }
 
+// SignedMessageToClient simple message with known author
 type SignedMessageToClient struct {
 	Message string `json:"message"`
 	Author  string `json:"author"`
