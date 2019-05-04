188e5ec1e6f73ef42cb5e0bad901821d95003756
GDVFox <gaaveer@gmail.com>
2019-05-05 01:19:28 +0300

    [ADD] Configure

--- go.mod
+++ go.mod
@@ -3,15 +3,15 @@ module github.com/HotCodeGroup/warscript-chat
 go 1.12
 
 require (
-	github.com/HotCodeGroup/warscript-imageserver v0.0.0-20190401214303-52ec590a471d
-	github.com/HotCodeGroup/warscript-utils v0.0.0-20190428100847-cde473768f6e
+	github.com/HotCodeGroup/warscript-utils v0.0.0-20190504211345-1a79c3bbaa15
 	github.com/gorilla/handlers v1.4.0
 	github.com/gorilla/mux v1.7.1
 	github.com/gorilla/websocket v1.4.0
+	github.com/hashicorp/consul/api v1.0.1
+	github.com/hashicorp/vault/api v1.0.1
 	github.com/jackc/pgx v3.3.0+incompatible
-	github.com/jcftang/logentriesrus v0.0.0-20170718201731-9bf66587097e
 	github.com/pkg/errors v0.8.1
 	github.com/sirupsen/logrus v1.4.1
-	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
+	google.golang.org/appengine v1.4.0 // indirect
 	google.golang.org/grpc v1.20.1
 )
--- main.go
+++ main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
+	"github.com/HotCodeGroup/warscript-utils/balancer"
 	"github.com/HotCodeGroup/warscript-utils/logging"
+	"github.com/HotCodeGroup/warscript-utils/middlewares"
 	"github.com/HotCodeGroup/warscript-utils/models"
 	"github.com/HotCodeGroup/warscript-utils/postgresql"
 
-	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
-	"google.golang.org/grpc"
+
+	consulapi "github.com/hashicorp/consul/api"
+	vaultapi "github.com/hashicorp/vault/api"
 )
 
 var logger *logrus.Logger
 var authGPRC models.AuthClient
 
+//nolint: gocyclo
 func main() {
 	var err error
 	logger, err = logging.NewLogger(os.Stdout, os.Getenv("LOGENTRIESRUS_TOKEN"))
@@ -26,44 +32,77 @@ func main() {
 		return
 	}
 
-	pgxConn, err = postgresql.Connect(os.Getenv("DB_USER"), os.Getenv("DB_PASS"),
-		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	config := consulapi.DefaultConfig()
+	config.Address = os.Getenv("CONSUL_ADDR")
+	consul, err := consulapi.NewClient(config)
+	if err != nil {
+		logger.Errorf("can not connect consul service: %s", err)
+		return
+	}
+
+	vaultConfig := vaultapi.DefaultConfig()
+	vaultConfig.Address = os.Getenv("VAULT_ADDR")
+	vault, err := vaultapi.NewClient(vaultConfig)
+	if err != nil {
+		logger.Errorf("can not connect vault service: %s", err)
+		return
+	}
+	vault.SetToken(os.Getenv("VAULT_TOKEN"))
+
+	httpPort, _, err := balancer.GetPorts("warscript-chat/bounds", "warscript-bots", consul)
+	if err != nil {
+		logger.Errorf("can not find empry port: %s", err)
+		return
+	}
+
+	// получаем конфиг на постгрес и стартуем
+	postgreConf, err := vault.Logical().Read("warscript-chat/postgres")
+	if err != nil || postgreConf == nil || len(postgreConf.Warnings) != 0 {
+		logger.Errorf("can read warscript-chat/postges key: %+v; %+v", err, postgreConf)
+		return
+	}
+	pgxConn, err = postgresql.Connect(postgreConf.Data["user"].(string), postgreConf.Data["pass"].(string),
+		postgreConf.Data["host"].(string), postgreConf.Data["port"].(string), postgreConf.Data["database"].(string))
 	if err != nil {
 		logger.Errorf("can not connect to postgresql database: %s", err.Error())
 		return
 	}
 	defer pgxConn.Close()
 
-	authGPRCConn, err := grpc.Dial(
-		os.Getenv("AUTH_ADDRESS"),
-		grpc.WithInsecure(),
-	)
+	authGPRCConn, err := balancer.ConnectClient(consul, "warscript-users-grpc")
 	if err != nil {
-		logger.Errorf("can not connect to auth grpc")
+		logger.Errorf("can not connect to auth grpc: %s", err.Error())
 		return
 	}
 	defer authGPRCConn.Close()
-
 	authGPRC = models.NewAuthClient(authGPRCConn)
 
 	h := NewHub()
 	go h.run()
+
+	httpServiceID := fmt.Sprintf("warscript-chat-http:%d", httpPort)
+	err = consul.Agent().ServiceRegister(&consulapi.AgentServiceRegistration{
+		ID:      httpServiceID,
+		Name:    "warscript-chat-http",
+		Port:    httpPort,
+		Address: "127.0.0.1",
+	})
+	defer func() {
+		err = consul.Agent().ServiceDeregister(httpServiceID)
+		if err != nil {
+			logger.Errorf("can not derigister http service: %s", err)
+		}
+		logger.Info("successfully derigister http service")
+	}()
 
 	r := mux.NewRouter().PathPrefix("/v1").Subrouter()
 	r.HandleFunc("/chat/connect", func(w http.ResponseWriter, r *http.Request) {
 		ConnectChat(h, w, r)
 	}).Methods("GET")
 
-	corsMiddleware := handlers.CORS(
-		handlers.AllowedOrigins([]string{os.Getenv("CORS_HOST")}),
-		handlers.AllowedMethods([]string{"POST", "GET", "PUT", "DELETE"}),
-		handlers.AllowedHeaders([]string{"Content-Type"}),
-		handlers.AllowCredentials(),
-	)
-
-	httpPort := os.Getenv("PORT")
-	logger.Infof("Chat HTTP service successfully started at port %s", httpPort)
-	err = http.ListenAndServe(":"+httpPort, corsMiddleware(r))
+	logger.Infof("Chat HTTP service successfully started at port %d", httpPort)
+	err = http.ListenAndServe(":"+strconv.Itoa(httpPort),
+		middlewares.RecoverMiddleware(middlewares.AccessLogMiddleware(r, logger), logger))
 	if err != nil {
 		logger.Errorf("cant start main server. err: %s", err.Error())
 		return
